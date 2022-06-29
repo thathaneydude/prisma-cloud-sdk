a553219e13091cdba6f6317ee5e318dd36d436f1
Ryan Haney <rhaney@paloaltonetworks.com>
2022-06-29 14:42:49 -0500

    Adding config search

--- cspm/resource_test.go
+++ cspm/resource_test.go
@@ -1,27 +1,22 @@
 package cspm
 
-import (
-	"github.com/stretchr/testify/assert"
-	"github.com/thathaneydude/prisma-cloud-sdk/internal"
-	bc "github.com/thathaneydude/prisma-cloud-sdk/internal/client"
-	"net/http"
-	"testing"
-)
-
-func TestCspmClient_GetResource(t *testing.T) {
-	teardown := setup()
-	defer teardown()
-	cspmClient, err := NewCSPMClient(&ClientOptions{
-		ApiUrl:     server.URL,
-		SslVerify:  false,
-		Schema:     "http",
-		MaxRetries: 3,
-	})
-	assert.Nil(t, err)
-	mux.HandleFunc(resourceEndpoint, func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set(bc.ContentTypeHeader, bc.ApplicationJSON)
-		w.Header().Set(internal.AuthHeader, "foo")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"licenseType":"enterprise","marketplace":"Palo Alto Networks Marketplace","startTs":1539027762524,"endTs":1667174400000,"twistlockUrl":"https://us-east1.cloud.twistlock.com/12345"}`))
-	})
-}
+//func TestCspmClient_GetResource(t *testing.T) {
+//	teardown := setup()
+//	defer teardown()
+//	cspmClient, err := NewCSPMClient(&ClientOptions{
+//		ApiUrl:     server.URL,
+//		SslVerify:  false,
+//		Schema:     "http",
+//		MaxRetries: 3,
+//	})
+//	assert.Nil(t, err)
+//	mux.HandleFunc(resourceEndpoint, func(w http.ResponseWriter, r *http.Request) {
+//		w.Header().Set(bc.ContentTypeHeader, bc.ApplicationJSON)
+//		w.Header().Set(internal.AuthHeader, "foo")
+//		w.WriteHeader(http.StatusOK)
+//		w.Write([]byte(``))
+//	})
+//	resource, err := cspmClient.GetResource("foo")
+//	assert.Nil(t, err)
+//	assert.Equal(t)
+//}
---
+++ cspm/search.go
@@ -0,0 +1,66 @@
+package cspm
+
+import "github.com/thathaneydude/prisma-cloud-sdk/internal"
+
+const searchConfigEndpoint = "/search/config"
+
+func (c *CspmClient) SearchConfig(req ConfigSearchRequest) (*SearchConfigResponse, error) {
+	var searchResponse SearchConfigResponse
+	err := c.postWithResponseInterface(searchConfigEndpoint, internal.ToBytes(req), &searchResponse)
+	if err != nil {
+		return nil, err
+	}
+	return &searchResponse, nil
+}
+
+type ConfigSearchRequest struct {
+	Id                string `json:"id"`
+	Limit             int    `json:"limit"`
+	Query             string `json:"query"`
+	SearchDescription string `json:"searchDescription"`
+	SearchName        string `json:"searchName"`
+	Sort              []struct {
+		Direction string `json:"direction"`
+		Field     string `json:"field"`
+	} `json:"sort"`
+	TimeRange struct {
+		RelativeTimeType string `json:"relativeTimeType"`
+		Type             string `json:"type"`
+		Value            struct {
+			Amount int    `json:"amount"`
+			Unit   string `json:"unit"`
+		} `json:"value"`
+	} `json:"timeRange"`
+	WithResourceJson bool `json:"withResourceJson"`
+}
+
+type SearchConfigResponse struct {
+	AlertId        string                 `json:"alertId"`
+	Async          bool                   `json:"async"`
+	AsyncResultUrl string                 `json:"asyncResultUrl"`
+	CloudType      string                 `json:"cloudType"`
+	Cursor         int                    `json:"cursor"`
+	Data           map[string]interface{} `json:"data"`
+	Default        bool                   `json:"default"`
+	Description    string                 `json:"description"`
+	Filters        []struct {
+		Name     string `json:"name"`
+		Operator string `json:"operator"`
+		Value    string `json:"value"`
+	} `json:"filters"`
+	GroupBy         []string `json:"groupBy"`
+	Id              string   `json:"id"`
+	Name            string   `json:"name"`
+	Query           string   `json:"query"`
+	Saved           bool     `json:"saved"`
+	SearchType      string   `json:"searchType"`
+	TimeGranularity string   `json:"timeGranularity"`
+	TimeRange       struct {
+		RelativeTimeType string `json:"relativeTimeType"`
+		Type             string `json:"type"`
+		Value            struct {
+			Amount int    `json:"amount"`
+			Unit   string `json:"unit"`
+		} `json:"value"`
+	} `json:"timeRange"`
+}
