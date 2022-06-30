749b55a1af7a373b077e32d5922a70a2edf1ac63
Ryan Haney <rhaney@paloaltonetworks.com>
2022-06-30 16:45:53 -0500

    fixing response struct

--- cspm/search.go
+++ cspm/search.go
@@ -1,6 +1,8 @@
 package cspm
 
-import "github.com/thathaneydude/prisma-cloud-sdk/internal"
+import (
+	"github.com/thathaneydude/prisma-cloud-sdk/internal"
+)
 
 const searchConfigEndpoint = "/search/config"
 
@@ -35,32 +37,25 @@ type SearchConfigRequest struct {
 }
 
 type SearchConfigResponse struct {
-	AlertId        string                 `json:"alertId"`
-	Async          bool                   `json:"async"`
-	AsyncResultUrl string                 `json:"asyncResultUrl"`
-	CloudType      string                 `json:"cloudType"`
-	Cursor         int                    `json:"cursor"`
-	Data           map[string]interface{} `json:"data"`
-	Default        bool                   `json:"default"`
-	Description    string                 `json:"description"`
-	Filters        []struct {
-		Name     string `json:"name"`
-		Operator string `json:"operator"`
-		Value    string `json:"value"`
-	} `json:"filters"`
-	GroupBy         []string `json:"groupBy"`
-	Id              string   `json:"id"`
-	Name            string   `json:"name"`
-	Query           string   `json:"query"`
-	Saved           bool     `json:"saved"`
-	SearchType      string   `json:"searchType"`
-	TimeGranularity string   `json:"timeGranularity"`
-	TimeRange       struct {
-		RelativeTimeType string `json:"relativeTimeType"`
-		Type             string `json:"type"`
-		Value            struct {
-			Amount int    `json:"amount"`
+	CloudType   string `json:"cloudType"`
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	SearchType  string `json:"searchType"`
+	Saved       bool   `json:"saved"`
+	TimeRange   struct {
+		Type  string `json:"type"`
+		Value struct {
 			Unit   string `json:"unit"`
+			Amount int    `json:"amount"`
 		} `json:"value"`
+		RelativeTimeType string `json:"relativeTimeType"`
 	} `json:"timeRange"`
+	Query string `json:"query"`
+	Data  struct {
+		TotalRows       int                      `json:"totalRows"`
+		Items           []map[string]interface{} `json:"items"`
+		NextPageToken   string                   `json:"nextPageToken"`
+		HeuristicSearch bool                     `json:"heuristicSearch"`
+	} `json:"data"`
 }
